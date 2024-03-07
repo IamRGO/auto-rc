f02684349667245107d9e5133c7404417af110a3
Ricky Go <rickygo@rickys-mbp.home>
2024-03-06 21:56:57 -0400

    added debugging info

--- drive.py
+++ drive.py
@@ -15,8 +15,8 @@ import data
 print("Staring camera...")
 
 camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 128)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 96)
 camera.set(cv2.CAP_PROP_FPS, 30)
 
 
@@ -37,7 +37,7 @@ arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=5)
 
 input("press enter to begin driving...")
 while True:
-  print("driving...")
+  print("taking a picture...")
   _, frame = camera.read()  # read the camera frame
   image = cv2.resize(frame, (128, 96))
   hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -53,6 +53,7 @@ while True:
     data.parse_image(result)
   ]
 
+  print("running a prediction...")
   result = model.predict(
     np.array(input_list, dtype=np.float32),
     verbose=0,
@@ -60,22 +61,15 @@ while True:
 
   result = result[0]
 
-  print(result[0], result)
+  print(result)
 
-  steering_val = np.interp(result[1], [0.0, 1.0], [40, 130])
-  throttle_val = np.interp(result[2], [0.0, 1.0], [90, 250])
+  steering_val = np.interp(result[0], [0.0, 1.0], [40, 130])
+  throttle_val = np.interp(result[1], [0.0, 1.0], [90, 250])
 
   if result[2] == 0:
       throttle_val = 0
 
   message = "D" + str(steering_val) + " " + str(throttle_val)
   arduino.write(message.encode("UTF-8"))
-
-  # if steering in range(45, 55):
-  #   print("Turn STRAIGHT")
-  # elif steering in range(0, 45):
-  #   print("Turn LEFT")
-  # else:
-  #   print("Turn RIGHT")
 
   time.sleep(0.1)
