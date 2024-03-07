7e80a3687f10e20396c3b537c520b8b5f688b7d1
Ricky Go <rickygo@rickys-mbp.home>
2024-03-07 13:02:01 -0400

    decreased rc reciever top speed

--- rc_receiver/rc_receiver.ino
+++ rc_receiver/rc_receiver.ino
@@ -55,7 +55,7 @@ void loop() {
       if (data[1] > 980) {
         data[1] = 980;
       }
-      throttle_val = map(data[1], 520, 980, 90, 250);
+      throttle_val = map(data[1], 520, 980, 90, 180);
       analogWrite(motor_a, throttle_val);
       analogWrite(motor_b, 0);
     }
@@ -64,7 +64,7 @@ void loop() {
       if (data[1] < 300) {
         data[1] = 300;
       }
-      reverse_throttle_val = map(-1 * data[1], -480, -300, 90, 250);
+      reverse_throttle_val = map(-1 * data[1], -480, -300, 90, 180);
       analogWrite(motor_b, reverse_throttle_val);
       analogWrite(motor_a, 0);
     }
