738af9098420e048d59b928da1e8f5deaa06d45e
Ricky Go <lionkingricky@gmail.com>
2024-03-08 14:58:10 -0400

    lower record image resolution

--- record.py
+++ record.py
@@ -3,12 +3,12 @@ import serial
 import time
 
 camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 camera.set(cv2.CAP_PROP_FPS, 30)
 
-# arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=19200, timeout=5)
-arduino = serial.Serial(port='/dev/cu.usbserial-1420', baudrate=19200, timeout=5)
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=19200, timeout=5)
+# arduino = serial.Serial(port='/dev/cu.usbserial-1420', baudrate=19200, timeout=5)
 time.sleep(2)
 print("Sending data...")
 
