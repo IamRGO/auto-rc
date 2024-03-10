6697d61e3431bf283da9b59973d38686f62d311b
Ricky Go <lionkingricky@gmail.com>
2024-03-10 16:24:32 -0300

    increase resolution

--- data.py
+++ data.py
@@ -25,11 +25,11 @@ def parse_image(image):
   rgb_image = mask_image(image)
   img_tensor = tf.convert_to_tensor(rgb_image, dtype=tf.float32)
   img_gray = tf.image.rgb_to_grayscale(img_tensor)
-  img_resized = tf.image.resize_with_pad(img_gray, 160, 120)
+  img_resized = tf.image.resize_with_pad(img_gray, 320, 240)
   return img_resized
 
 def mask_image(image):
-  image = cv2.resize(image, (160, 120))
+  image = cv2.resize(image, (320, 240))
 
   hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
   light_yellow = np.array([20, 50, 140])
