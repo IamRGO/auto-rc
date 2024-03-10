1a2e77fd2699407f24278ba043284b10b87f6b73
Michael Go <ggmichaelgo@gmail.com>
2024-03-09 21:23:30 -0400

    less verbose logging from main

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ import data
 
 list_of_files = [f for f in listdir('temp') if isfile(join('temp', f))]
 
+count = 0
+total = len(list_of_files)
+
 for file in list_of_files:
+  count += 1
+
+  if count % 100 == 0:
+    print("Processing", file, "(", count, "/", total, ")")
+
   if "png" in file:
     image = cv2.imread('temp/' + file)
     result = data.mask_image(image)
     cv2.imwrite('processed_temp/' + file, result)
-
-    print('temp/result_' + file)
-    print('---')
-
   else:
     shutil.move('temp/' + file, 'processed_temp/' + file)
-    print("data moved")
