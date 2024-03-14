bd241f24c30bb938da21da56de37adc47a2b90a1
Michael Go <ggmichaelgo@gmail.com>
2024-03-13 23:43:23 -0300

    stream with FPS info

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import time
 import cv2
 import asyncio
 import websockets
@@ -14,18 +15,31 @@ async def handle_connection(websocket, path):
 def get_frames():
     global camera
 
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    frame_count = 0
+    last_fps = 0
+    last_time = time.time()
+
     while True:
-        image = camera.capture_array("lores")
-        rgb = cv2.cvtColor(image, cv2.COLOR_YUV420p2RGB)
+        image = camera.capture_array()
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         frame = imutils.resize(rgb, width=100)
+        frame_count += 1
+
+        cv2.putText(frame, f'FPS: {last_fps}', (10, 30), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
         _, buffer = cv2.imencode('.png', frame)
         frame = base64.b64encode(buffer)
         yield b'data:image/png;base64,' + frame
 
+        if time.time() - last_time >= 1:
+            last_fps = frame_count
+            frame_count = 0
+            last_time = time.time()
+
 # Start video capture
 camera = Picamera2()
 config = camera.create_preview_configuration(
-    lores={ "size": (320, 240) },
+    main={ "size": (int(3280/4), int(2464/4)) },
 )
 camera.configure(config)
 camera.start()
