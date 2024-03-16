2c20f6e85abc924d57f6b531ef16f07c216f437d
Ricky Go <lionkingricky@gmail.com>
2024-03-16 19:13:45 -0300

    resize recording image to 320px wide

--- record.py
+++ record.py
@@ -2,21 +2,35 @@ import cv2
 import serial
 import time
 
-camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-camera.set(cv2.CAP_PROP_FPS, 30)
-camera.set(cv2.cap_prop_buffersize, 1)
+import imutils
+from picamera2 import Picamera2
+
+#camera = cv2.VideoCapture(0)
+#camera.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+#camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+#camera.set(cv2.CAP_PROP_FPS, 30)
+#camera.set(cv2.cap_prop_buffersize, 1)
+
+camera = Picamera2()
+config = camera.create_preview_configuration(
+    main={ "size": (int(3280/4), int(2464/4)) },
+)
+camera.configure(config)
+camera.start()
 
 arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=5)
 # arduino = serial.Serial(port='/dev/cu.usbserial-1420', baudrate=9600, timeout=5)
+
 time.sleep(2)
 print("Sending data...")
 
 count = int(input("where do you want to start?"))
 
 while True:
-    _, frame = camera.grab()  # read the camera frame
+    image = camera.capture_array()
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(rgb, width=320)
+
     image_filename = f'/home/pi/usb/temp/frame_{count}.png'
 
     arduino.write(b"stats")
