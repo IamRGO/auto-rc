13b59290d1f07dfce24d488c188ccbd091f7eb16
Michael Go <ggmichaelgo@gmail.com>
2024-03-17 13:27:03 -0300

    remodel

--- model.py
+++ model.py
@@ -9,12 +9,12 @@ def create_model():
         MaxPooling2D(),
         Conv2D(64, 3, padding='same', activation='relu'),
         MaxPooling2D(),
-        Conv2D(128, 3, padding='same', activation='relu'),
+        Conv2D(64, 3, padding='same', activation='relu'),
         MaxPooling2D(),
 
         Flatten(),
         Dense(240, activation='relu'),
-        Dense(80, activation='relu'),
+        Dense(100, activation='relu'),
         Dense(20, activation='relu'),
         Dense(1, activation='sigmoid')
     ])
