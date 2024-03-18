8ea74beba0faec09bb665a8e81275a4416ce9cd4
Michael Go <ggmichaelgo@gmail.com>
2024-03-18 19:21:23 -0300

    small brain

--- model.py
+++ model.py
@@ -4,15 +4,7 @@ from tensorflow.keras.layers import Dense, Dropout, MaxPooling2D, Conv2D, Flatte
 def create_model():
     model = Sequential([
         Rescaling(1.0/255, input_shape=(120, 160, 1)),
-        Conv2D(16, 3, padding='same', activation='relu'),
-        MaxPooling2D(),
-        Conv2D(32, 3, padding='same', activation='relu'),
-        MaxPooling2D(),
         Conv2D(64, 3, padding='same', activation='relu'),
-        MaxPooling2D(),
-        Conv2D(128, 3, padding='same', activation='relu'),
-        MaxPooling2D(),
-        Conv2D(256, 3, padding='same', activation='relu'),
         MaxPooling2D(),
 
         Flatten(),
