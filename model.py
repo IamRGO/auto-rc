0a3bbd0b311c6aeaa16a63ec1cd28b7a4b1ccb03
Michael Go <ggmichaelgo@gmail.com>
2024-03-17 22:07:34 -0300

    less conv2d

--- model.py
+++ model.py
@@ -6,15 +6,10 @@ def create_model():
         Rescaling(1.0/255, input_shape=(120, 160, 1)),
         Conv2D(16, 3, padding='same', activation='relu'),
         MaxPooling2D(),
-        Conv2D(32, 3, padding='same', activation='relu'),
-        MaxPooling2D(),
-        Conv2D(64, 3, padding='same', activation='relu'),
-        MaxPooling2D(),
 
         Flatten(),
         # before was 1164, 512, 240
         Dense(32, activation='relu'),
-        Dense(16, activation='relu'),
         Dense(16, activation='relu'),
         Dense(1, activation='sigmoid')
     ])
