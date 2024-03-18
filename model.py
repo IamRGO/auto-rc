a17a704fdcb34560c1397ab32bc3ba263e466726
Michael Go <ggmichaelgo@gmail.com>
2024-03-18 00:56:03 -0300

    smaller brain

--- model.py
+++ model.py
@@ -12,9 +12,10 @@ def create_model():
         MaxPooling2D(),
         Conv2D(128, 3, padding='same', activation='relu'),
         MaxPooling2D(),
+        Conv2D(256, 3, padding='same', activation='relu'),
+        MaxPooling2D(),
 
         Flatten(),
-        Dense(512, activation='relu'),
         Dense(240, activation='relu'),
         Dense(32, activation='relu'),
         Dense(1, activation='sigmoid')
